c64caca15d73249d14da750fe5f9a30ae7aad37d
ZhaklinaTefova <119459579+ZhaklinaTefova@users.noreply.github.com>
2023-01-23 21:36:15 +0000

    Update exercise7.js

--- Exercise_7/exercise7.js
+++ Exercise_7/exercise7.js
@@ -4,22 +4,46 @@ Null, undefined, NaN, “” and false (JavaScript falsy values).
 Create a function that will work for every array in the world :)
  and it will remove all the falsy values from the array.*/
 
-let myArray = [2, 3, "New", "Hi", "four", "", false, undefined, NaN, null];
-function myFunction(removedFalsyValues) {
-  removedFalsyValues = [];
-  for (let i = 0; i < myArray.length; i++) {
+function myFunction1(array) {
+  let removedFalsyValues = [];
+  for (let i = 0; i < array.length; i++) {
     if (
-      myArray[i] !== null &&
-      myArray[i] !== "" &&
-      myArray[i] !== false &&
-      myArray[i] !== undefined &&
-      !Number.isNaN(myArray[i])
+      array[i] !== null &&
+      array[i] !== "" &&
+      array[i] !== false &&
+      array[i] !== undefined &&
+      !Number.isNaN(array[i])
     ) {
-      removedFalsyValues.push(myArray[i]);
+      removedFalsyValues.push(array[i]);
     }
   }
-  console.log(removedFalsyValues);
+  return removedFalsyValues;
 }
-myFunction();
+let myArray = [2, 3, "New", "Hi", "four", "", false, undefined, NaN, null];
+console.log(myFunction1(myArray));
 
 // !!variable = it will check if the value is truty or falsy
+
+
+function myFunction2(array) {
+  let removedFalsyValues = [];
+  for (let i = 0; i < array.length; i++) {
+    if (!!array[i]) {
+      removedFalsyValues.push(array[i]);
+    }
+  }
+  return removedFalsyValues;
+}
+console.log(myFunction2(myArray));
+
+
+
+function myFunction3(array) {
+  let removedFalsyValues = [];
+  for (let i = 0; i < array.length; i++) {
+   removedFalsyValues = array.filter(Boolean) 
+  }
+  return removedFalsyValues;
+}
+console.log(myFunction3(myArray));
+
